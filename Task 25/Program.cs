a3cd15aeb88cb852107890792d8e12dc029bb2a1
morpatims <kverteletski@mail.ru>
2023-06-20 16:39:55 +0300

    Add Tasks 25, 27, 29

---
+++ Task 25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+DegreeOfIncrease();
+void DegreeOfIncrease()
+{
+    Console.Write("Enter the value for A: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter the value for B: ");
+    int B = Convert.ToInt32(Console.ReadLine());
+
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result *= A;
+    }
+
+    Console.WriteLine("The result is: " + result);
+}
---
+++ Task 27/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+PromptNumber();
+void PromptNumber()
+{
+    Console.Write("Enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int sum = GetDigitSum(number);
+    Console.WriteLine("Sum of digits: " + sum);
+}
+
+int GetDigitSum(int number)
+{
+    int sum = 0;
+    number = Math.Abs(number);
+    while (number != 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+
+    return sum;
+}
